e72a4525979b7c15010679a065b5b27f70bf7b85
Robert Gulewicz <nayael@wp.pl>
2018-09-07 23:33:02 +0200

    Add app skeleton

---
+++ src/app/containers/App/index.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+
+export namespace App {
+    export interface Props {
+    }
+}
+
+export class App extends React.Component<App.Props> {
+    static defaultProps: Partial<App.Props> = {
+    };
+
+    constructor(props: App.Props, context?: any) {
+        super(props, context);
+    }
+
+    render() {
+        return (
+            <div>
+                App
+            </div>
+        );
+    }
+}
